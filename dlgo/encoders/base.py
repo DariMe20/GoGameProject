99f8b13e739576cd5100d6a1d73418261540a6aa
DariMe20 <dariamesesan@yahoo.com>
2024-02-10 14:14:46 +0200

    Created 4 layer neuronal network using keras and training data from 40k MCTS simulations

--- dlgo/encoders/base.py
+++ dlgo/encoders/base.py
@@ -26,10 +26,3 @@ class Encoder:
     def shape(self):
         raise NotImplementedError()
 
-    # Aceasta functie permite crearea encoderelor folosind numele lor
-    def get_encoder_by_name(name, board_size):
-        if isinstance(board_size, int):
-            board_size = (board_size, board_size)
-        module = importlib.import_module('dlgo.encoders.' + name)
-        constructor = getattr(module, 'create')
-        return constructor(board_size)
