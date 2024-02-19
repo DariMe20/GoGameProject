d556d3dc2117d151d892dc4a121baa94bd852fd4
Daria <med6clj@bosch.com>
2024-02-19 13:12:28 +0200

    Changes made to agent in order to make it trainable

--- dlgo/agent/policy_agent.py
+++ dlgo/agent/policy_agent.py
@@ -1,26 +1,93 @@
-import h5py
+"""Policy gradient learning."""
 import numpy as np
-from keras import Sequential
-from keras.src.layers import Dense, Activation
-import os
-import dlgo
-import tensorflow as tf
-from dlgo import encoders, agent, kerasutil, goboard
+from keras import backend as K
+from keras.optimizers import SGD
+from keras.src.saving.saving_api import save_model
+
 from dlgo.agent.base import Agent
 from dlgo.agent.helpers import is_point_an_eye
-from dlgo.encoders.simple import SimpleEncoder
-from dlgo.goboard import GameState
-from dlgo.keras_networks import large_network
-from dlgo.rl.experience import ExperienceBuffer
-from dlgo.rl.experience_colector import ExperienceCollector
+from dlgo import encoders
+from dlgo import goboard
+from dlgo import kerasutil
+
+__all__ = [
+    'PolicyAgent',
+    'load_policy_agent',
+    'policy_gradient_loss',
+]
+
+
+# Keeping this around so we can read existing agents. But from now on
+# we'll use the built-in crossentropy loss.
+def policy_gradient_loss(y_true, y_pred):
+    clip_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+    loss = -1 * y_true * K.log(clip_pred)
+    return K.mean(K.sum(loss, axis=1))
+
+
+def normalize(x):
+    total = np.sum(x)
+    return x / total
+
 
 class PolicyAgent(Agent):
+    """An agent that uses a deep policy network to select moves."""
     def __init__(self, model, encoder):
-        super().__init__()
-        self.collector = None
+        Agent.__init__(self)
         self._model = model
         self._encoder = encoder
-        # self.color = color
+        self._collector = None
+        self._temperature = 0.5
+
+    def predict(self, game_state):
+        encoded_state = self._encoder.encode(game_state)
+        input_tensor = np.array([encoded_state])
+        return self._model.predict(input_tensor)[0]
+
+    def set_temperature(self, temperature):
+        self._temperature = temperature
+
+    def set_collector(self, collector):
+        self._collector = collector
+
+    def select_move(self, game_state):
+        num_moves = self._encoder.board_width * self._encoder.board_height
+
+        board_tensor = self._encoder.encode(game_state)
+        x = np.array([board_tensor])
+
+        if np.random.random() < self._temperature:
+            # Explore random moves.
+            move_probs = np.ones(num_moves) / num_moves
+        else:
+            # Follow our current policy.
+            move_probs = self._model.predict(x)[0]
+
+        # Prevent move probs from getting stuck at 0 or 1.
+        eps = 1e-5
+        move_probs = np.clip(move_probs, eps, 1 - eps)
+        # Re-normalize to get another probability distribution.
+        move_probs = move_probs / np.sum(move_probs)
+
+        print_probs(move_probs, self._encoder.board_width, self._encoder.board_height)
+
+        # Turn the probabilities into a ranked list of moves.
+        candidates = np.arange(num_moves)
+        ranked_moves = np.random.choice(
+            candidates, num_moves, replace=False, p=move_probs)
+        for point_idx in ranked_moves:
+            point = self._encoder.decode_point_index(point_idx)
+            if game_state.is_valid_move(goboard.Move.play(point)) \
+                    and not is_point_an_eye(game_state.board,point, game_state.next_player) \
+                    and not game_state.is_move_on_edge(goboard.Move.play(point)):
+                if self._collector is not None:
+                    self._collector.record_decision(
+                        state=board_tensor,
+                        action=point_idx
+                    )
+                return goboard.Move.play(point)
+        # No legal, non-self-destructive moves less.
+        return goboard.Move.pass_turn()
 
     def serialize(self, h5file):
         h5file.create_group('encoder')
@@ -28,75 +95,34 @@ class PolicyAgent(Agent):
         h5file['encoder'].attrs['board_width'] = self._encoder.board_width
         h5file['encoder'].attrs['board_height'] = self._encoder.board_height
         h5file.create_group('model')
-        kerasutil.save_model_to_hdf5_group(self._model, h5file['model'])
+        save_model(self._model, h5file)
 
-    def select_move(self, game_state):
-        # codificare tabla
-        board_tensor = self._encoder.encode(game_state)
-        X = np.array([board_tensor])
+    def train(self, experience, lr=0.009, clipnorm=1.0, batch_size=712):
+        opt = SGD(learning_rate=lr, clipnorm=clipnorm)
+        self._model.compile(loss='categorical_crossentropy', optimizer=opt)
 
-        # generare probabilitati
-        move_probs = self._model.predict(X)[0]
-        move_probs = clip_probs(move_probs)
-        print_probs(move_probs, 9, 9)
-
-        # preluare numar total de mutari
+        n = experience.states.shape[0]
+        # Translate the actions/rewards.
         num_moves = self._encoder.board_width * self._encoder.board_height
+        y = np.zeros((n, num_moves))
+        for i in range(n):
+            action = experience.actions[i]
+            reward = experience.rewards[i]
+            y[i][action] = reward
 
-        # aranjare si alegere mutari candidat
-        candidates = np.arange(num_moves)
-        ranked_moves = np.random.choice(candidates, num_moves, replace=False, p=move_probs)
-
-        # decodare mutari posibile
-        for point_idx in ranked_moves:
-            point = self._encoder.decode_point_index(point_idx)
-
-            # verificare mutare
-            move = goboard.Move.play(point)
-            is_valid = game_state.is_valid_move(move)
-            is_an_eye = is_point_an_eye(game_state.board, point, game_state.next_player)
-            is_on_edge = game_state.is_move_on_edge(move)
-
-            # alege mutarea
-            if is_valid and (not is_an_eye):
-
-                # notific collectorul ca agentul a ales o mutare
-                if self.collector is not None:
-                    self.collector.record_decision(
-                        state=board_tensor,
-                        action=point_idx
-                    )
-
-                return goboard.Move.play(point)
-
-        # pass daca nu exista mutari valide/bune
-        return goboard.Move.pass_turn()
-
-    def set_collector(self, collector):
-        self.collector = collector
-
-
-def print_probs(move_probs, board_width, board_height):
-    i = 0
-    for row in range(board_height):
-        row_formatted = []
-        for col in range(board_width):
-            row_formatted.append('{:.3f}'.format(move_probs[i]))
-            i += 1
-        print(' '.join(row_formatted))
-
-def clip_probs(original_probs):
-    min_p = 1e-5
-    max_p = 1 - min_p
-    clipped_probs = np.clip(original_probs, min_p, max_p)
-    clipped_probs = clipped_probs / np.sum(clipped_probs)
-    return clipped_probs
+        self._model.fit(
+            experience.states, y,
+            batch_size=batch_size,
+            epochs=1)
 
 
 def load_policy_agent(h5file):
     model = kerasutil.load_model_from_hdf5_group(
-        h5file['model'])
+        h5file['model'],
+        custom_objects={'policy_gradient_loss': policy_gradient_loss})
     encoder_name = h5file['encoder'].attrs['name']
+    if not isinstance(encoder_name, str):
+        encoder_name = encoder_name.decode('ascii')
     board_width = h5file['encoder'].attrs['board_width']
     board_height = h5file['encoder'].attrs['board_height']
     encoder = encoders.get_encoder_by_name(
@@ -104,3 +130,13 @@ def load_policy_agent(h5file):
         (board_width, board_height))
     return PolicyAgent(model, encoder)
 
+# Afișează probabilitățile într-un format lizibil
+def print_probs(move_probs, board_width, board_height):
+    i = 0
+    for row in range(board_height):
+        row_formatted = []
+        for col in range(board_width):
+            mi = move_probs[i] * 100
+            row_formatted.append('{:.2f}'.format(mi) + "%")
+            i += 1
+        print(' '.join(row_formatted))
