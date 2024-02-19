182d94a502da99e7fe60972437caa0ccb0839809
Daria <med6clj@bosch.com>
2024-02-19 13:14:11 +0200

    Added imports

---
+++ dlgo/agent/__init__.py
@@ -0,0 +1,4 @@
+from .base import *
+from .policy_agent import *
+from .predict import *
+from .naive import *
--- dlgo/rl/__init__.py
+++ dlgo/rl/__init__.py
@@ -1,1 +1,4 @@
-import keras
+from .experience import *
+from .policy_training import *
+from .experience_colector import *
+from .rl_with_policy_gradients import *
