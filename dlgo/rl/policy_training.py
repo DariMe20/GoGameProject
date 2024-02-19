a37244d80974ed9a554413967887d6e76c78459b
Daria <med6clj@bosch.com>
2024-02-19 13:11:39 +0200

    Made experience collector combined based on agents results to collect training data

--- dlgo/rl/policy_training.py
+++ dlgo/rl/policy_training.py
@@ -1,22 +1,11 @@
+import os
 import h5py
-import keras
-import numpy as np
-import tensorflow as tf
-
-from keras.src.regularizers import l2
 from keras.src.saving.saving_api import load_model
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import Adam
-
-from dlgo import agent, rl
-from dlgo.agent import policy_agent
 from dlgo.agent.policy_agent import PolicyAgent
-from dlgo.agent.predict import DeepLearningAgent
 from dlgo.encoders.simple import SimpleEncoder
 from dlgo.goboard import GameState
 from dlgo.gotypes import Player
-from dlgo.keras_networks import large_network
 from dlgo.rl.experience_colector import ExperienceCollector
 from dlgo.rl import experience
 
@@ -25,17 +14,24 @@ board_size = 9
 encoder = SimpleEncoder((board_size, board_size))
 model = Sequential()
 
-model_path = 'C:\\Users\\MED6CLJ\\Desktop\\FSEGA_IE\\Licenta\\GoGameProject\\dlgo\\keras_networks\\black_agent_model3.h5'
-model_p = load_model(model_path)
+model_path1 = 'C:\\Users\\MED6CLJ\\Desktop\\FSEGA_IE\\Licenta\\GoGameProject\\dlgo\\keras_networks\\model_gradient2.h5'
+model_p1 = load_model(model_path1)
 
-agent1 = PolicyAgent(model_p, encoder)
-agent2 = PolicyAgent(model_p, encoder)
+model_path2 = 'C:\\Users\\MED6CLJ\\Desktop\\FSEGA_IE\\Licenta\\GoGameProject\\dlgo\\keras_networks\\model_gradient1.h5'
+model_p2 = load_model(model_path2)
+
+agent1 = PolicyAgent(model_p1, encoder)
+agent2 = PolicyAgent(model_p2, encoder)
 
 collector1 = ExperienceCollector()
 collector2 = ExperienceCollector()
 
 agent1.set_collector(collector1)
 agent2.set_collector(collector2)
+
+black_wins = 0
+white_wins = 0
+
 
 # METODA PENTRU JOC SI COLECTARE A EXPERIENTELOR
 def play_game(agent_black, agent_white, board_size):
@@ -67,20 +63,22 @@ for episode in range(num_episodes):
     if game_record == Player.black:
         collector1.complete_episode(reward=1)
         collector2.complete_episode(reward=-1)
+        black_wins += 1
     else:
         collector2.complete_episode(reward=1)
         collector1.complete_episode(reward=-1)
+        white_wins += 1
     print(f"Finished episode {episode} with success!")
 
 
-experience = experience.combine_experience([collector1,collector2])
-experience_filename = 'C:\\Users\\MED6CLJ\\Desktop\\FSEGA_IE\\Licenta\\GoGameProject\\dlgo\\experience_files\\experience1.h5'
+experience_combined = experience.combine_experience([collector1, collector2])
+experience_filename = 'C:\\Users\\MED6CLJ\\Desktop\\FSEGA_IE\\Licenta\\GoGameProject\\dlgo\\experience_files\\experience20.h5'
 with h5py.File(experience_filename, 'w') as experience_outf:
-    experience.serialize(experience_outf)
+    experience_combined.serialize(experience_outf)
 
+print(f"Black wins: {black_wins} \ {num_episodes} \n White wins: {white_wins} \ {num_episodes}")
 # SALVARE MODELE
 # model_policy.save('C:\\Users\\MED6CLJ\\Desktop\\FSEGA_IE\\Licenta\\GoGameProject\\dlgo\\keras_networks\\black_agent_model5.h5')
 # model_white.save('C:\\DARIA\\1.FSEGA\\LICENTA\\GoGameProject\\dlgo\\keras_networks\\white_agent_model.h5')
 
 # model.save('C:\\DARIA\\1.FSEGA\\LICENTA\\GoGameProject\\dlgo\\keras_networks\\model3.h5')
-
