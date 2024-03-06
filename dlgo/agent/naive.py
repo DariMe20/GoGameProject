1be7ddadc4e141e5b80f14f2143670fb172ca22f
Daria <med6clj@bosch.com>
2024-03-06 13:10:33 +0200

    Moved helpers.py in utils

--- dlgo/agent/naive.py
+++ dlgo/agent/naive.py
@@ -2,9 +2,12 @@ import random
 from dlgo.agent.base import Agent
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 from dlgo.agent.helpers import is_point_an_eye
 from dlgo.game_rules_implementation.goboard import Move
 =======
+=======
+>>>>>>> 603e82c (Moved helpers.py in utils)
 =======
 >>>>>>> 603e82c (Moved helpers.py in utils)
 from utils.helpers import is_point_an_eye
