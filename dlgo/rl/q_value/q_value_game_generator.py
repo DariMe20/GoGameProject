234a1543e2f41b86bfb3dd7ccfe613b2f0d8d753
Daria <med6clj@bosch.com>
2024-03-07 22:07:33 +0200

    Created game generator class for q_value that inherits from base class

--- dlgo/rl/q_value/q_value_game_generator.py
+++ dlgo/rl/q_value/q_value_game_generator.py
@@ -1,34 +1,50 @@
-from keras import Sequential
+import os
+
 from keras.src.saving.saving_api import load_model
 
 from agent import QAgent
 from dlgo.encoders.simple import SimpleEncoder
 from dlgo.rl.base_game_generator import GameGenerator
 
+# INITIALISE VARIABLES
+
 board_size = 9
 encoder = SimpleEncoder((board_size, board_size))
-model = Sequential()
 
-path_Q1 = 'C:\\Users\\MED6CLJ\\Desktop\\FSEGA_IE\\Licenta\\GoGameProject\\dlgo\\rl\\q_value\\q_models\\model_Q2.h5'
+# MODEL PATHS
+path_Q1 = os.path.join('..', '..', 'rl', 'q_value', 'q_models', 'model_Q2.h5')
+path_Q2 = os.path.join('..', '..', 'rl', 'q_value', 'q_models', 'model_Q2_V2.h5')
+
+# DATA SAVING PATHS
+output_folder_outer = os.path.join('..', '..', 'json_data', 'q_value', 'game_generator')
+filename_outer = os.path.join(output_folder_outer, 'game_generator_summary.json')
+experience_dir_outer = os.path.join('..', 'q_value', 'q_value_experience_files')
+
+# LOAD MODELS
 model_Q1 = load_model(path_Q1)
-path_Q2 = 'C:\\Users\\MED6CLJ\\Desktop\\FSEGA_IE\\Licenta\\GoGameProject\\dlgo\\rl\\q_value\\q_models\\model_Q2_V2.h5'
 model_Q2 = load_model(path_Q2)
 
+# CREATE AGENTS
 agent1 = QAgent(model_Q1, encoder)
 agent2 = QAgent(model_Q2, encoder)
 
-output_folder = r'../../json_data/q_value/game_generator'
-filename = r'../../json_data/q_value/game_generator/game_generator_summary.json'
-experience_dir = r'../q_value/q_value_experience_files'
+# GET AGENT NAMES FROM MODEL FILES
+black_name = os.path.splitext(os.path.basename(path_Q1))[0]
+white_name = os.path.splitext(os.path.basename(path_Q2))[0]
 
-black_name = "model Q2"
-white_name = "model Q2_v2"
+# SET AGENT EXPLORATION FACTOR
 temp_black = 0.02
 temp_white = 0.02
 
-q_game_generator = GameGenerator(agent_black=agent1, agent_white=agent2,
-                                 black_key=black_name, white_key=white_name,
-                                 temp_black=temp_black, temp_white=temp_white,
-                                 output_folder=output_folder, filename=filename, experience_directory=experience_dir)
 
-q_game_generator.generate_games(1, 9)
+class QValueGameGenerator(GameGenerator):
+    def __init__(self, agent_black=agent1, agent_white=agent2,
+                 black_key=black_name, white_key=white_name,
+                 black_temp=temp_black, white_temp=temp_white,
+                 output_folder=output_folder_outer, filename=filename_outer, experience_directory=experience_dir_outer):
+        super().__init__(agent_black, agent_white, black_key, white_key, black_temp, white_temp, output_folder,
+                         filename, experience_directory)
+
+
+q_game_generator = QValueGameGenerator()
+q_game_generator.generate_games(1, encoder.board_width)
