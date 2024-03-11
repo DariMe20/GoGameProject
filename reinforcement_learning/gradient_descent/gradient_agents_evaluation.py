d0d49f2460a6ddcaf54143ae38e68601152b06ec
Daria <med6clj@bosch.com>
2024-03-11 15:07:47 +0200

    Modified gradient evaluator file to create class that inherits from GameEvaluator

--- reinforcement_learning/gradient_descent/gradient_agents_evaluation.py
+++ reinforcement_learning/gradient_descent/gradient_agents_evaluation.py
@@ -1,102 +1,44 @@
 import os
 
-import utils.constants
-from dlgo.game_rules_implementation.Player import Player
-from dlgo.game_rules_implementation.goboard import GameState
-from utils import data_file_manipulator
+from keras.src.saving.saving_api import load_model
 
-# Inițializarea agenților
-agent1_key = "Policy Gradient 5"
-agent2_key = "Policy Gradient 4"
-agent1 = utils.constants.BOTS[agent1_key]
-agent2 = utils.constants.BOTS[agent2_key]
+from agent import PolicyAgent
+from dlgo.encoders.simple import SimpleEncoder
+from reinforcement_learning.base_generate_train_evaluate.base_game_evaluator import GameEvaluator
 
-# INITIALIZARE DATE
-black_wins = 0
-white_wins = 0
-total_black_score = 0
-total_white_score = 0
+# INITIALISE VARIABLES
+
 board_size = 9
-num_episodes = 100
-game_results = []
-output_folder = r'../../json_data/gradient_descent'
-filename = r'../../json_data/gradient_descent/game_evaluation_summary.json'
-os.makedirs(output_folder, exist_ok=True)
+encoder = SimpleEncoder((board_size, board_size))
+
+# MODEL PATHS
+path_G1 = os.path.join('gradient_descent_models', 'model_Gradient1.h5')
+path_G2 = os.path.join('gradient_descent_models', 'model_Gradient2.h5')
+
+# LOAD MODELS
+model_G1 = load_model(path_G1)
+model_G2 = load_model(path_G2)
+
+# CREATE AGENTS
+agent1 = PolicyAgent(model_G1, encoder)
+agent2 = PolicyAgent(model_G2, encoder)
+
+# GET AGENT NAMES FROM MODEL FILES
+black_name = os.path.splitext(os.path.basename(path_G1))[0]
+white_name = os.path.splitext(os.path.basename(path_G2))[0]
+
+# DATA SAVING PATHS
+output_folder_outer = os.path.join('..', '..', 'dlgo', 'json_data', 'gradient_descent')
+filename_outer = os.path.join('..', '..', 'dlgo', 'json_data', 'gradient_descent', 'game_evaluation_summary.json')
 
 
-# METODA PENTRU GENERARE DE JOC
-def play_game(agent_black, agent_white, board_size):
-    state = GameState.new_game(board_size)
-    agents = {
-        Player.black: agent_black,
-        Player.white: agent_white
-        }
-
-    while not state.is_over():
-        next_player = state.next_player
-        agent_next = agents[next_player]
-        move = agent_next.select_move(state)
-        state = state.apply_move(move)
-
-    winner, scores = state.evaluate_territory()
-    black_score = scores[Player.black]
-    white_score = scores[Player.white]
-    difference = abs(black_score - white_score)
-
-    # Returnare date folosite in json
-    return {
-        'black_agent': agent1_key,
-        'white_agent': agent2_key,
-        'winner': state.winner(),
-        'black_territory': black_score,
-        'white_territory': white_score,
-        'territory_difference': difference
-        }
+class GradientEvaluator(GameEvaluator):
+    def __init__(self, agent_black=agent1, agent_white=agent2, black_key=black_name, white_key=white_name,
+                 output_folder=output_folder_outer, filename=filename_outer):
+        super().__init__(agent_black, agent_white, black_key, white_key, output_folder, filename)
 
 
-# Parcurgere episoade
-for episode in range(num_episodes):
-    print(f"Started episode: {episode}")
-    # Preluare informatii dupa joc
-    game_info = play_game(agent1, agent2, board_size)
-    if game_info['winner'] == Player.black:
-        black_wins += 1
-    else:
-        white_wins += 1
-
-    # Adaugare informatii la game_result
-    score = {'black': black_wins, 'white': white_wins}
-    game_results.append({
-        'episode': episode,
-        'winner': str(game_info['winner']),
-        'current_score': score,
-        'black_agent': str(game_info['black_agent']),
-        'white_agent': str(game_info['white_agent']),
-        'black_score': str(game_info['black_territory']),
-        'white_score': str(game_info['white_territory']),
-        'territory_difference': str(game_info['territory_difference'])
-        })
-
-    total_black_score += game_info['black_territory']
-    total_white_score += game_info['white_territory']
-    print(f"Finished episode {episode} with success!")
-
-# GENERAREA DATELOR SI POPULAREA FISIERELOR JSON
-average_black_score = total_black_score / num_episodes
-average_white_score = total_white_score / num_episodes
-print(f"Black wins: {black_wins} / {num_episodes} \nWhite wins: {white_wins} / {num_episodes}")
-
-summary_info = {
-    'Episodes': num_episodes,
-    'Black_agent': agent1_key,
-    'White_agent': agent2_key,
-    'Black_wins': black_wins,
-    'White_wins': white_wins,
-    'Average_black_score': average_black_score,
-    'Average_white_score': average_white_score
-    }
-
-# SALVARE DATE IN FISIERE JSON
-data_file_manipulator.generate_filename(agent1_key, agent2_key, output_folder)
-data_file_manipulator.save_all_games_info(game_results, summary_info, agent1_key, agent2_key, output_folder)
-data_file_manipulator.save_summary_info(summary_info, filename)
+# GIVE NUMBER OF SIMULATIONS AND RUN GENERATOR
+NUM_EPISODES = 1
+gradient_game_evaluator = GradientEvaluator()
+gradient_game_evaluator.generate_games(NUM_EPISODES, encoder.board_width)
